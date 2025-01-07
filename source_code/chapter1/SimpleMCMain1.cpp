701bfb405482ad471094fd9670cd0b6460c6a242
dadi1 <narayan.shimanoe@gmail.com>
2025-01-07 18:21:08 -0300

    SimpleMonteCarlo() implemented in SimpleMCMain1.cpp

---
+++ source_code/chapter1/SimpleMCMain1.cpp
@@ -0,0 +1,31 @@
+#include <Random1.h>
+#include <iostream>
+#include <cmath>
+
+double SimpleMonteCarlo(double Expiry,
+                        double Strike,
+                        double Spot,
+                        double Vol,
+                        double r,
+                        long NumberOfPaths) 
+{
+    double variance = Vol * Vol * Expiry;
+    double rootVariance = std::sqrt(variance);
+    double itoCorrection = -0.5 * variance;
+
+    double movedSpot = Spot * std::exp(r * Expiry + itoCorrection);
+    double thisSpot;
+    double runningSum = 0;
+
+    for (int i =0; i < NumberOfPaths; i++) {
+        double thisGaussian = GetOneGaussianByBoxMuller();
+        double thisSpot = movedSpot * std::exp(rootVariance * thisGaussian);
+        double thisPayoff = thisSpot - Strike;
+        thisPayoff = thisPayoff > 0 ? thisPayoff : 0;
+        runningSum += thisPayoff;
+    }
+
+    double mean = runningSum / NumberOfPaths;
+    mean += std::exp(-r * Expiry);
+    return mean;
+}
