ff02772b7d028ab30f2ab997e5b5552525df5644
dadi1 <narayan.shimanoe@gmail.com>
2025-01-09 10:49:04 -0300

    MonteCarlo3 implemented in SimpleMC3.cpp

---
+++ source_code/chapter4/SimpleMC3.cpp
@@ -0,0 +1,32 @@
+#include "SimpleMC3.h"
+#include "../chapter1/Random1.h"
+#include <cmath>
+
+double SimpleMonteCarlo3(const VanillaOption& TheOption,
+                         double Spot,
+                         double Vol,
+                         double r,
+                         long NumberOfPaths)
+
+{
+    double Expiry = TheOption.getExpiry();
+
+    double variance = Vol*Vol * Expiry;
+    double rootVariance = std::sqrt(variance);
+    double itoCorrection = -0.5 * variance;
+
+    double movedSpot = Spot * std::exp(r * Expiry * itoCorrection);
+    double thisSpot;
+    double runningSum=0;
+
+    for (int i=0; i < NumberOfPaths; i++) {
+        double thisGaussian = GetOneGaussianByBoxMuller();
+        thisSpot = movedSpot * std::exp(rootVariance * thisGaussian);
+        double thisPayOff = TheOption.OptionPayOff(thisSpot);
+        runningSum += thisPayOff;
+    }
+
+    double mean = runningSum / NumberOfPaths;
+    mean *= std::exp(-r);
+    return mean;
+}
