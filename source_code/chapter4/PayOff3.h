188f8b19d0c5972f562d41a9f41ea5e9c324164d
dadi1 <narayan.shimanoe@gmail.com>
2025-01-15 16:07:31 -0300

    lcass PayOffimplemented

---
+++ source_code/chapter4/PayOff3.h
@@ -0,0 +1,15 @@
+#ifndef PAYOFF3_H
+#define PAYOFF3_H
+
+class PayOff
+{
+public:
+    PayOff(){};
+
+    virtual double operator() (double Spot) const=0;
+    virtual ~PayOff(){}
+    virtual PayOff* clone() const=0;
+
+private:
+
+};
