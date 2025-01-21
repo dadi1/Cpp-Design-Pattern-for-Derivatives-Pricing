8cef9ad54079e9569ecfd2d3316ad333eccb6aff
dadi1 <narayan.shimanoe@gmail.com>
2025-01-21 10:35:22 -0300

    class ConvergenceTable implemented in source file

--- source_code/chapter5/ConvergenceTable.cpp
+++ source_code/chapter5/ConvergenceTable.cpp
@@ -8,4 +8,47 @@ ConvergenceTable::ConvergenceTable(const
     PathsDone = 0;
 }
 
-Statis
+StatisticsMC* ConvergenceTable::clone() const
+{
+    return new ConvergenceTable(*this);
+}
+
+void ConvergenceTable::DumpOneResult(double result) 
+{
+    Inner->DumpOneResult(result);
+    PathsDone++;
+
+    if (PathsDone == StoppingPoint)
+    {
+        StoppingPoint *= 2;
+        std::vector<std::vector<double> >
+            thisResult(Inner->GetResultSoFar());
+        
+        for (int i = 0; i < thisResult.size();  i++)
+        {
+            thisResult[i].push_back(PathsDone);
+            ResultSoFar.push_back(thisResult[i]);
+        }
+    }
+
+    return;
+}
+
+std::vector<std::vector<double> >
+                ConvergenceTable::GetResultSoFar() const
+{
+    std::vector<std::vector<double> > tmp(ResultSoFar);
+
+    if (PathsDone * 2 != StoppingPoint)
+    {
+        std::vector<std::vector<double> >
+                thisResult =(Inner->GetResultSoFar());
+
+        for (int i = 0; i < thisResult.size(); i++)
+        {
+            thisResult[i].push_back(PathsDone);
+            tmp.push_back(thisResult[i]);
+        }   
+    }
+    return tmp;
+}
