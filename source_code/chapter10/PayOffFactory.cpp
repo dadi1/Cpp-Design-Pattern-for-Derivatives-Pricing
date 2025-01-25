782723df0323aced5d3bcf8fe3a788d24c124b93
dadi1 <narayan.shimanoe@gmail.com>
2025-01-25 20:16:31 -0300

    bug finded

---
+++ source_code/chapter10/PayOffFactory.cpp
@@ -0,0 +1,9 @@
+#include "PayOffFactory.h"
+#include <iostream>
+
+void PayOffFactory::RegisterPayOff(string PayOffId,
+              CreatePayOffFunction CreatorFunction)
+{
+    TheCreateFunction.insert(pair<string,CreatePayOffFunction>
+                                )
+}
